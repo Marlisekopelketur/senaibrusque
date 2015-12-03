b705fb3367d7e5a048058d16dcc670f58b08f580
Aluno <Aluno@10.1.20.88>
2015-12-02 23:17:34 -0200

    Notícias

--- js/noticia.js
+++ js/noticia.js
@@ -2,10 +2,24 @@
  * Responsável pela busca AJAX de apenas uma notícia
  */
 
+var id = 0;
+$('document').ready(function () {
+    $('#noticias button').click(function () {
+        //Botão clicado
+        if ($.isNumeric($(this).attr('id'))) {
+            id = $(this).attr('id');
+            console.log(id);
+            //Reconstruição do identificador da notícia
+            noticiaID = '#noticia' + id;
+            console.log(noticiaID);
+            //AJAX método GET
+            $.get(
+                    "noticia1.php",
+                    function (retorno) {
+                        $(noticiaID).html(retorno);
+                    }
+            );
 
-$('document') .ready(function (){
-  $('.panel-body button').click(function(){ 
-  console.log()
-  });  
-    
+        }
+    });
 });
